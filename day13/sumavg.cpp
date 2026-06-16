fcc95d26a75eec18a76c6a6b5cdaad45cae7850b
spec500 <girlisro026@gmail.com>
2026-06-16 23:32:14 +0530

    Update sumavg.cpp

---
+++ day13/sumavg.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int n;
+    cin >> n;
+
+    int arr[n];
+    int sum = 0;
+
+    for(int i = 0; i < n; i++)
+    {
+        cin >> arr[i];
+        sum += arr[i];
+    }
+
+    cout << "Sum = " << sum << endl;
+    cout << "Average = " << (float)sum / n;
+
+    return 0;
+}
