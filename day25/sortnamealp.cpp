f0497abcc715f20fe9260a8ba83d9cd84c5fecf9
spec500 <girlisro026@gmail.com>
2026-06-25 11:14:29 +0530

    Update sortnamealp.cpp

---
+++ day25/sortnamealp.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+#include <algorithm>
+using namespace std;
+
+int main() {
+
+    int n;
+    cin >> n;
+
+    string names[100];
+
+    for(int i=0;i<n;i++)
+        cin >> names[i];
+
+    sort(names, names + n);
+
+    for(int i=0;i<n;i++)
+        cout << names[i] << endl;
+
+    return 0;
+}
