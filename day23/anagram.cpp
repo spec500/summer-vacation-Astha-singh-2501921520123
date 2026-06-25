3dc2e194ce5bf3d2c8b2201c1389b89b37a72950
spec500 <girlisro026@gmail.com>
2026-06-25 11:07:28 +0530

    Update anagram.cpp

---
+++ day23/anagram.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+#include <algorithm>
+using namespace std;
+
+int main() {
+    string str1, str2;
+
+    cin >> str1 >> str2;
+
+    sort(str1.begin(), str1.end());
+    sort(str2.begin(), str2.end());
+
+    if(str1 == str2)
+        cout << "Anagram";
+    else
+        cout << "Not Anagram";
+
+    return 0;
+}
